69ce3c2becefff6ae8c3f357199181d32911b4c9
Adarsh <8432akshatupadhyay@gmail.com>
2026-08-06 10:46:51 +0530

    Remove old practice files, keep hello-world and greatest-of-three

--- newfile.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    vector<int> v = {5, 2, 8, 1};
-    sort(v.begin(), v.end());
-    for (int x : v) cout << x << " ";
-    return 0;
-}
